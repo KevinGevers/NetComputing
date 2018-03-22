30f051b04c56b718d3c07e448db23e24c7837e49
NicoStegeman <nicostegeman89@outlook.com>
2018-03-22 09:05:42 +0100

    Started error messages

--- Manager/manager_app.py
+++ Manager/manager_app.py
@@ -1,16 +1,47 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 
 app = Flask(__name__)
 
 parking_spaces = {
     'total' : 20,
-    'availible' : 5,
-    'reserved' : 2
+    'available' : 5,
+    'reserved' : 1
 }
+
+reservations = [
+    {
+        'client_id' : 'test',
+        'start_time' : '2012-04-23T18:25:43.511Z'
+    }
+]
 
 @app.route('/parkingspaces', methods=['GET'])
 def get_parkingspaces():
+    if len(parking_spaces) == 0:
+        abort(404)
     return jsonify(parking_spaces)
+
+@app.route('/reservations', methods=['GET'])
+def get_reservation():
+    if len(reservations) == 0:
+        abort(404)
+    return jsonify({'reservations': reservations})
+
+def get_reservation(client_id):
+    reservation = [reservation for reservation in reservations if reservation['client_id'] == client_id]
+    if len(reservation) == 0:
+        abort(404)
+    return jsonify({'reservation': reservation[0]})
+
+def create_reservation():
+    return 'Not implemented yet'
+
+def delete_reservation():
+    return 'Not implemented yet'
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
